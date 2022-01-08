1bf1a22ed02f71a0f46df9f4b453a74e85d4d9c5
OLEG <schastliviy.sluchay@ukr.net>
2022-01-08 05:13:10 +0200

    0.4

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-if (window.location.pathname === "/") {
-  getFreshOFTrending();
-}
+getFreshOfTrending();
+getTrending();
 
-async function getFreshOFTrending() {
+async function getFreshOfTrending() {
   const resultOfFetch = await fetch(
     "https://content.guardianapis.com/search?q=trending&show-tags=all&page-size=20&show-fields=all&order-by=relevance&api-key=5ef33414-1934-47dc-9892-5d09ab7c00da"
   )
@@ -31,17 +30,72 @@ async function getFreshOFTrending() {
   const text = document.querySelector(".main_news__text");
   text.textContent = freshTrending.fields.bodyText;
 
-  const publicDate = new Date(freshTrending.webPublicationDate);
+  const date = document.querySelector(".main_news__date");
+  date.textContent = formatDate(freshTrending.webPublicationDate);
+
+  const image = document.querySelector(".main_news__image");
+  const imageUrl = freshTrending.fields.main.split('"')[5];
+  image.setAttribute("src", imageUrl);
+}
+
+async function getTrending() {
+  const resultOfFetch = await fetch(
+    "https://content.guardianapis.com/search?q=trending&show-tags=all&page-size=20&show-fields=all&order-by=relevance&api-key=5ef33414-1934-47dc-9892-5d09ab7c00da"
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      return response;
+    });
+  const articles = resultOfFetch.response.results;
+  let fresh = 0;
+  let dontNeededIndex = 0;
+
+  articles.map((article, index) => {
+    const date = new Date(article.webPublicationDate);
+    if (fresh < date) {
+      fresh = date;
+      dontNeededIndex = index;
+    }
+  });
+
+  articles.splice(dontNeededIndex, 1);
+
+  const list = document.querySelector(".other_news__list");
+
+  articles.map((i) => {
+    const article = i.fields;
+    console.log(article);
+    const { headline, bodyText, firstPublicationDate } = article;
+
+    const img = article.main.split('"')[5];
+    const item = document.createElement("li");
+    item.classList.add("other_news__item");
+    item.insertAdjacentHTML(
+      "afterbegin",
+
+      `<div class="other_news__image_area">
+        <img class="other_news__image" src=${img}></img>
+      </div >
+
+      <h3 class='other_news__title'>${headline}</h3>
+      <div class='other_news__text_area'>
+     
+          <p class='other_news__text'>${bodyText}</p>
+      </div>
+      <div class='other_news__date_area'>
+          <p class='other_news__date'>${formatDate(firstPublicationDate)}</p>
+          <a class='other_news__link' href='#'>Read more</a>
+      </div>`
+    );
+    list.appendChild(item);
+  });
+}
+
+function formatDate(date) {
+  const publicDate = new Date(date);
   const formatDate = publicDate.toDateString();
   const formatDateArr = formatDate.split(" ");
   formatDateArr.splice(0, 1);
   formatDateArr[1] = formatDateArr[1] + "th";
-  const resultDate = formatDateArr.join(" ");
-  const date = document.querySelector(".main_news__date");
-  date.textContent = resultDate;
-
-  const image = document.querySelector(".main_news__image");
-
-  const imageUrl = freshTrending.fields.main.split('"')[5];
-  image.setAttribute("src", imageUrl);
+  return formatDateArr.join(" ");
 }
