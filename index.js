534ae2f1a5ffced29683f827202e72628351f476
OLEG <schastliviy.sluchay@ukr.net>
2022-01-08 15:11:38 +0200

    0.5

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-getFreshOfTrending();
-getTrending();
+location.hash = "/trending";
+
+window.onhashchange = () => {
+  if (location.hash === "#/trending") {
+    getFreshOfTrending();
+    getTrending();
+  } else {
+    const mainDiv = document.querySelector("#action_area");
+    mainDiv.innerHTML = "";
+    getArticleById(getPath());
+  }
+};
 
 async function getFreshOfTrending() {
   const resultOfFetch = await fetch(
@@ -23,7 +33,6 @@ async function getFreshOfTrending() {
   });
 
   const freshTrending = articles[neededIndex];
-
   const title = document.querySelector(".main_news__title");
   title.textContent = freshTrending.fields.headline;
 
@@ -36,6 +45,14 @@ async function getFreshOfTrending() {
   const image = document.querySelector(".main_news__image");
   const imageUrl = freshTrending.fields.main.split('"')[5];
   image.setAttribute("src", imageUrl);
+
+  const link = document.querySelector(".main_news__link");
+  link.setAttribute("id", `/${freshTrending.id}`);
+
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    location.hash = freshTrending.id;
+  });
 }
 
 async function getTrending() {
@@ -64,7 +81,6 @@ async function getTrending() {
 
   articles.map((i) => {
     const article = i.fields;
-    console.log(article);
     const { headline, bodyText, firstPublicationDate } = article;
 
     const img = article.main.split('"')[5];
@@ -72,8 +88,8 @@ async function getTrending() {
     item.classList.add("other_news__item");
     item.insertAdjacentHTML(
       "afterbegin",
-
-      `<div class="other_news__image_area">
+      `<article>
+      <div class="other_news__image_area">
         <img class="other_news__image" src=${img}></img>
       </div >
 
@@ -84,11 +100,46 @@ async function getTrending() {
       </div>
       <div class='other_news__date_area'>
           <p class='other_news__date'>${formatDate(firstPublicationDate)}</p>
-          <a class='other_news__link' href='#'>Read more</a>
-      </div>`
+          <a class='other_news__link' href='#' id = "${i.id}";>Read more</a>
+      </div></article>`
     );
     list.appendChild(item);
   });
+}
+
+async function getArticleById(id) {
+  const result = await fetch(
+    `https://content.guardianapis.com/search?q=${id}&show-tags=all&page-size=20&show-fields=all&order-by=relevance&api-key=5ef33414-1934-47dc-9892-5d09ab7c00da`
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      // console.log(response.response.results.find((item) => item.id === id));
+      return response.response.results.find((item) => item.id === id);
+    });
+  // console.log(result);
+  const article = result.fields;
+  console.log(article);
+
+  const imageUrl = article.main.split('"')[5];
+  console.log(imageUrl);
+
+  const mainDiv = document.querySelector("#action_area");
+  mainDiv.insertAdjacentHTML(
+    "afterbegin",
+    `<div class='main_article__image_area'> 
+      <img class="main_article__image" src=${imageUrl} />
+    </div>`
+  );
+}
+
+// -----------------------------------------------------
+
+function getPath() {
+  const hash = location.hash;
+  const arr = hash.split("");
+  arr.splice(0, 1);
+  const path = arr.join("");
+  return path;
 }
 
 function formatDate(date) {
@@ -96,6 +147,16 @@ function formatDate(date) {
   const formatDate = publicDate.toDateString();
   const formatDateArr = formatDate.split(" ");
   formatDateArr.splice(0, 1);
-  formatDateArr[1] = formatDateArr[1] + "th";
+
+  if (formatDateArr[1][1] === "1") {
+    formatDateArr[1] = formatDateArr[1] + "st";
+  } else if (formatDateArr[1][1] === "2") {
+    formatDateArr[1] = formatDateArr[1] + "nd";
+  } else if (formatDateArr[1][1] === "3") {
+    formatDateArr[1] = formatDateArr[1] + "rd";
+  } else {
+    formatDateArr[1] = formatDateArr[1] + "th";
+  }
+
   return formatDateArr.join(" ");
 }
